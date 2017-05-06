04b5dd2708c50634132a47e1f8904d9091cadac8
Robert Peele <robert.s.peele@gmail.com>
2017-05-05 20:20:44 -0400

    WIP: switch to lighter static DI pattern.

---
+++ CalendarSystem.Persistence/IPersistence.fs
@@ -0,0 +1,30 @@
+﻿namespace CalendarSystem.Persistence
+open CalendarSystem.Persistence.Calendar
+open CalendarSystem.Persistence.Membership
+
+type IPersistence =
+    abstract member Users : IUserPersistence
+    abstract member Sessions : ISessionPersistence
+    abstract member CalendarEvents : ICalendarEventPersistence
+
+[<AutoOpen>]
+module Implementation =
+    let mutable private instance = None
+
+    module Setup =
+        let usePersistence persistence =
+            match instance with
+            | None -> instance <- persistence
+            | Some _ -> bug "Persistence implementation should be set only once, at program initialization"
+
+    let persistence =
+        let i : IPersistence Lazy =
+            lazy
+                match instance with
+                | Some i -> i
+                | None -> bug "Persistence implementation was not set (call usePersistence at program initialization)"
+        { new IPersistence with
+            member __.Users = i.Value.Users
+            member __.Sessions = i.Value.Sessions
+            member __.CalendarEvents = i.Value.CalendarEvents
+        }
---
+++ Utilities/BugException.fs
@@ -0,0 +1,15 @@
+﻿namespace Utilities
+open System
+
+/// Represents an exception that should never be thrown.
+/// If it's thrown, it means somebody screwed up and the program needs to be fixed.
+type BugException(msg : string) =
+    // If I had my way, this class would be in the framework already, and some exceptions
+    // like NullReferenceException and IndexOutOfRangeException would inherit from it.
+    // Others, like SecurityException and FileNotFoundException are *not* necessarily indicative of bugs
+    // and are in fact expected to occur now and then in a correctly running system, so they would not inherit it.
+    inherit Exception(msg)
+
+[<AutoOpen>]
+module BugUtilities =
+    let inline bug msg = raise <| BugException(msg)
